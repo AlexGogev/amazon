0b0d23284bc267b9943101f310190ee7f2a5a61a
AlexGogev <70771051+AlexGogev@users.noreply.github.com>
2022-05-26 12:23:06 +0100

    Update settings.py

--- amazon/settings.py
+++ amazon/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w)b6f(4q6p_$57@o!b481^(ia%%$k_n^u^p)p%r(&gh+(e2u!h'
+SECRET_KEY = os.environ["key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
